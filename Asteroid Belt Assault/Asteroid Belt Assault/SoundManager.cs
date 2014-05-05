10974c3e6282c53dee13f3ff636c98bff9cd755e
Malachi <mtoomer81@gmail.com>
2014-05-05 10:10:36 -0400

    liuniou

--- Asteroid Belt Assault/Asteroid Belt Assault/SoundManager.cs
+++ Asteroid Belt Assault/Asteroid Belt Assault/SoundManager.cs
@@ -18,6 +18,7 @@ namespace Asteroid_Belt_Assault
 
         private static SoundEffect playerShot;
         private static SoundEffect enemyShot;
+        private static SoundEffect BGmusic;
 
         private static Random rand = new Random();
 
@@ -27,6 +28,7 @@ namespace Asteroid_Belt_Assault
             {
                 playerShot = content.Load<SoundEffect>(@"Sounds\Shot1");
                 enemyShot = content.Load<SoundEffect>(@"Sounds\Shot2");
+                BGmusic = content.Load<SoundEffect>(@"Sounds\BGmusic");
 
                 for (int x = 1; x <= explosionCount; x++)
                 {
